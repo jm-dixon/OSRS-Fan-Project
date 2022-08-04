ee1e28025e19b494bc44f6effb85189e51a4d29e
Joe Dixon <joe@crtr.co.uk>
2022-08-04 17:09:30 +0100

    added new buttons for alternate account types on highscores page, changes to styling to match for consistency across the site

--- components/SearchResult.vue
+++ components/SearchResult.vue
@@ -77,6 +77,7 @@ export default {
   name: 'SearchResult',
   props: {
     playerToSearch: String,
+    accountType: String
   },
   data() {
     return {
--- pages/highscores.vue
+++ pages/highscores.vue
@@ -1,31 +1,49 @@
 <template>
   <div class="highscoresPage">
     <div class="pageContainer">
+      <p class="subPageTitle">
+        <b>
+          {{ pageHeader }}
+        </b>
+      </p>
+      <p class="statisticsSubTitle">
+        {{ dataSource }}
+      </p>
       <div class="searchContainer">
-        <input id="playerInput" class="playerSearch" placeholder="Search Player..." />
-
-        <button class="searchButton" type="button" @click="this.getPlayerData">
-          {{ searchButtonText }}
-        </button>
-
-        <button class="clearButton" type="button" @click="this.clearSearch">
-          {{ clearButtonText }}
-        </button>
+        <div class="highscoresSearch">
+          <input id="playerInput" :placeholder="this.searchPlaceholder" />
+        </div>
+        <div class="highscoreButtons">
+          <button class="searchButton" type="button" @click="this.getPlayerData">
+            Search
+          </button>
+          <button class="conditionalButton" type="button" @click="this.clearSearch">
+            Clear
+          </button>
+        </div>
       </div>
-
       <div class="alternateSearches">
-        <div class="alternateButton">
-          <button type="button">Ironmen ></button>
+        <div>
+          <button type="button" @click="updateSearchParameters">Ironmen</button>
         </div>
-        <div class="alternateButton">
-           <button type="button">Ironmen (H) ></button>
+        <div>
+          <button type="button" @click="updateSearchParameters">Hardcore Ironmen</button>
         </div>
-        <div class="alternateButton">
-          <button type="button">Ironmen (U) ></button>
+        <div>
+          <button type="button" @click="updateSearchParameters">Ultimate Ironmen</button>
+        </div>
+        <div class="groupButtons">
+          <button class="groupButton" type="button" @click="updateSearchParameters">Group Ironmen</button>
+        </div>
+        <div class="groupButtons hardcoreGroup">
+          <button class="hardcoreButton" type="button" @click="updateSearchParameters">Hardcore Group Ironmen</button>
         </div>
       </div>
 
-      <search-result v-if="this.playerToSearch != null" :playerToSearch="this.playerToSearch" />
+      <search-result v-if="this.playerToSearch != null"
+        :playerToSearch="this.playerToSearch"
+        :accountType="this.accountType"
+      />
       <div v-else class="defaultResult">
         <p>
           No Player Name Entered
@@ -46,13 +64,12 @@ export default {
   },
   data() {
       return {
-        searchButtonText: 'Search',
-        clearButtonText: 'Clear',
-        playerToSearch: null
+        pageHeader: 'Highscores & Leaderboards',
+        dataSource: '* data pulled using the OSRS API *',
+        searchPlaceholder: 'Search Mains...',
+        playerToSearch: null,
+        accountType: null
       }
-  },
-  computed: {
-      /* No Computed */
   },
   methods: {
     getPlayerData() {
@@ -62,10 +79,31 @@ export default {
     clearSearch() {
       document.getElementById("playerInput").value = '';
       this.playerToSearch = null;
+      this.searchPlaceholder = 'Search Main Accounts...';
+      this.accountType = null;
+    },
+    updateSearchParameters(event) {
+      let value;
+      if (event) {
+        value = event.target.innerHTML;
+      }
+      if (value == 'Ironmen') {
+        this.searchPlaceholder = 'Search Ironmen...';
+        this.accountType = 'ironman';
+      } else if (value == 'Hardcore Ironmen') {
+        this.searchPlaceholder = 'Search Hardcore Ironmen...';
+        this.accountType = 'hardcore_ironman';
+      } else if (value.includes('mate')) {
+        this.searchPlaceholder = 'Search Ultimate Ironmen...';
+        this.accountType = 'ultimate';
+      } else if (value.includes('Hardcore Group')) {
+        this.searchPlaceholder = 'Search Hardcore Group Ironmen...';
+        this.accountType = '';
+      } else {
+        this.searchPlaceholder = 'Search Group Ironmen...';
+        this.accountType = '';
+      }
     }
-  },
-  mounted() {
-      /* No Mounted */
   },
 }
 </script>
