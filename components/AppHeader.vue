ca3f6c28944db1131c0110a8bb83d997b1ec0eaf
jm-dixon <jdixon101@hotmail.com>
2023-01-08 23:09:14 +0000

    adding URL checker to 'AppHeader.vue' which slightly changes the header depending on current route

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,5 +1,17 @@
 <template>
-    <div class="Header">
+  <div>
+    <div class="Header__notIndex" v-if="this.$route.name != 'index'">
+      <nuxt-link to='/'>
+          Home
+        </nuxt-link>
+        <nuxt-link to='/highscores'>
+          Highscores
+        </nuxt-link>
+        <nuxt-link to='/bestiary'>
+          Bestiary
+        </nuxt-link>
+    </div>
+    <div class="Header" v-if="this.$route.name === 'index'">
         <nuxt-link to='/'>
           Home
         </nuxt-link>
@@ -10,11 +22,12 @@
           Bestiary
         </nuxt-link>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'AppHeader'
+    name: 'AppHeader',
 }
 </script>
 
@@ -38,6 +51,27 @@ export default {
   transition: 0.8s ease;
 }
 .Header a:not(:hover) {
+  transition: 0.5s ease;
+}
+.Header__notIndex {
+  text-align: right;
+  display: flex;
+  width: 100%;
+  justify-content: flex-end;
+  background-color: var(--color-grey);
+}
+.Header__notIndex a {
+  text-decoration: none;
+  font-size: 1em;
+  color: var(--color-whiteSmoke);
+  padding: 1.5em 1.5em 1.5em 1.5em;
+  float: right;
+}
+.Header__notIndex a:hover {
+  color: var(--color-neruoscriptBlue);
+  transition: 0.8s ease;
+}
+.Header__notIndex a:not(:hover) {
   transition: 0.5s ease;
 }
 </style>
